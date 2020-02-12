9afb8d29e16ffaa65210ef80dae134d2caa8bd75
Petter Lopez <petterlopez003@gmail.com>
2020-02-11 20:51:41 -0500

    revision 1
    need fix on toggle

--- src/App.js
+++ src/App.js
@@ -9,16 +9,35 @@ constructor(){
   }
 }
 // this is where we are going to put our methods.
-increment = () => {
-  this.setState ({
-      count: this.state.count + 1
 
-  })
+increment = () => {
+  if ( this.state.count < 20){
+    this.setState ({
+        count: this.state.count + 1
+    })
+  }
+
 }
 decrement = () => {
+  if( this.state.count > 0){
+    this.setState ({
+      count: this.state.count - 1
+    })
+  }
+}
+
+reset = () => {
   this.setState ({
-    count: this.state.count - 1
+    count: this.state.count = 0
   })
+}
+toggleIn = () =>{
+  this.setState ({
+    count: this.state.count = 2
+  })
+}
+toggleDe = ()=>{
+
 }
 
   render(){
@@ -29,6 +48,10 @@ decrement = () => {
             <h1>{this.state.count}</h1>
             <button type="button" onClick={this.increment}>Increment</button>
             <button type="button" onClick={this.decrement}>Decrement</button>
+            <button type="button" onClick={this.reset}>Reset</button>
+            <button type="button" onClick={this.toggleIn}>Toggle increment</button>
+            <button type="button" onClick={this.toggleDe}>Toggle decrement</button>
+
           </div>
       </div>
     )
